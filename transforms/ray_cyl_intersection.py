1a8c4342ca9c8a429d3f791b11a3552f9b8bb361
Frankie Li <shiu.fai.li@gmail.com>
2020-08-29 23:34:27 -0700

    Added ray-plane intersection, implemented in Tensorflow. Not tested yet

--- transforms/ray_cyl_intersection.py
+++ transforms/ray_cyl_intersection.py
@@ -3,9 +3,10 @@
 """
 Created on Fri Oct 19 14:51:38 2018
 
-@author: bernier2
+@author: bernier2, sfli
 """
 import numpy as np
+import tensorflow as tf
 
 epsf = np.finfo(float).eps
 sqrt_epsf = np.sqrt(epsf)
@@ -28,22 +29,94 @@ def unit_vector(invec):
 
 def ray_plane(rays, rmat, tvec):
     """
+    tvec: Is the normal defining the plane.
+    rvec
     """
     nhat = rmat[:, 2].reshape(3, 1)
 
     rays = np.atleast_2d(rays)  # shape (npts, 3)
     npts = len(rays)
 
-    output = np.nan*np.ones_like(rays)
-
     numerator = np.tile(np.dot(tvec, nhat), npts)
     denominator = np.dot(rays, nhat).flatten()
-
     can_intersect = denominator < 0
-    u = np.tile(numerator[can_intersect]/denominator[can_intersect], (3, 1))
+
+    u = np.tile(numerator[can_intersect] / denominator[can_intersect], (3, 1))
+
+    output = np.nan*np.ones_like(rays)
     output[can_intersect, :] = rays[can_intersect] * u.T
 
     return np.dot(output - tvec, rmat)[:, :2]
+
+
+def tf_ray_plane(rays, rmat, tvec):
+    """
+    In general, the plane equation is defined by
+    $$\hat{n} \cdot (\vec{x} - \vec{x}_0)  = 0$$
+
+    where $\vec{x}_0$ defines the origin of rotation for the plane, $\hat{n}$
+    defines the plane normal.
+
+    For a given ray, $\vec{r}(t) = \vec{p}_0 + \hat{u} t$
+
+    the ray-plane intersection is defined by
+
+    $$t = \frac{\hat{n} \cdot (\vec{x}_0 - \vec{p}_0)} {\hat{n} \cdot \hat{u}} $$
+
+
+    To parameterize the detector plane, we will give the origin point a
+    translation vector.
+
+    This gives us
+
+    $$ \mathbf{R} \hat{n} \cdot (\vec{x} - (\vec{x}_0 + \vec{d}))  = 0$$
+
+    where $\vec{d}$ is the translation vector and $\mathbf{R}$ is the rotation
+    of the detector about its origin.
+
+
+    For this function, we'll take \vec{x_0} to be at the origin. We will define
+    the translation and rotation as `tvec` and `rmat`
+
+
+    :param tvec: Translation vector for the origin of the plane.
+    :param rmat: Rotation of the detector plane about its origin.
+    :param rays: Ray vector with $\vec{p}_0$ taken to be coordinate origin.
+
+    :return: The list of x, y intersection location of the plane.
+
+    As a convention, for tvec = [0, 0, 0], and R = np.eye(3), the detector
+    plane is located at the origin, with its normal pointing in the [0, 0, 1]
+    direction.
+
+    Similarly, by convention, the origin of the ray, $\vec{p}_0$ is taken to
+    be the origin as well.
+
+    """
+
+    # Detector normal, rotated about its origin.
+    nhat = tf.convert_to_tensor(rmat[:, 2], dtype='float32')
+    tvec = tf.convert_to_tensor(tvec, dtype='float32')
+
+    rays = np.atleast_2d(rays)
+    rays = tf.convert_to_tensor(rays, dtype='float32')  # shape (npts, 3)
+    npts = len(rays)
+    numerator = tf.repeat(tf.tensordot(tvec, nhat, axes=1), npts)
+    denominator = tf.tensordot(rays, nhat, axes=1)
+    tmp = numerator / denominator
+
+    # Using `d_censor` to replace the two lines below
+    # can_intersect = denominator < 0
+    # output[can_intersect, :] = rays[can_intersect] * u.T
+    d_censor = tf.scan(lambda a, x: 1.0 if x < 0 else np.nan, denominator)
+    tmp = tmp * d_censor
+    u = tf.stack([tmp, tmp, tmp], axis=1)
+    output = rays * u
+
+    # We only take the x, y, the detector plane coordinates.
+    return tf.tensordot(output - tvec, rmat, axes=[[1],[1]])[:, :2]
+
+
 
 
 def ray_cylinder(rays, radius, rmat, tvec):
@@ -57,7 +130,7 @@ def ray_cylinder(rays, radius, rmat, tvec):
     rays = np.atleast_2d(rays)  # shape (npts, 3)
     npts = len(rays)
 
-    output = np.nan*np.ones_like(rays)
+    output = np.nan * np.ones_like(rays)
 
     r_0 = rmat[:, 0]
     r_2 = rmat[:, 2]
@@ -99,3 +172,4 @@ def ray_cylinder(rays, radius, rmat, tvec):
             l_both[hits_inner_surf.flatten(), :],
             rmat)
     return output
+
